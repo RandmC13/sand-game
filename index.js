12941d67800289aee5075087e63c31b8039bbdff
RandmC13 <randmsamc137@gmail.com>
2021-01-08 00:04:08 +0000

    Added public as a static directory in expressjs

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
 	res.setHeader('Content-type', 'text/html');
-	fs.readFile('webfiles/index.html', (err, data) => {
+	fs.readFile('public/index.html', (err, data) => {
 		if (err) {throw error};
 		res.write(data);
 		res.end();
