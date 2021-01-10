8110600247b19b006bcf81e6752a030436e0a949
RandmC13 <randmsamc137@gmail.com>
2021-01-10 16:06:52 +0000

    Installed eslint as a dev dependency

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const fs = require('fs');
-const app = express();
-const port = 3000;
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+const port = 3000
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public')))
 
 app.get('/', (req, res) => {
-	res.setHeader('Content-type', 'text/html');
-	fs.readFile('public/index.html', (err, data) => {
-		if (err) {throw error};
-		res.write(data);
-		res.end();
-	});
-});
+  res.setHeader('Content-type', 'text/html')
+  fs.readFile('public/index.html', (err, data) => {
+    if (err) { throw err }
+    res.write(data)
+    res.end()
+  })
+})
 
 app.listen(port, () => {
-	console.log(`server started on port ${port}`);
-});
+  console.log(`server started on port ${port}`)
+})
