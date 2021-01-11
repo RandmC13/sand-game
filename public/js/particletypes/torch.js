e9b5b85240df5c5dfdf37c55d65599762ffa56ab
RandmC13 <randmsamc137@gmail.com>
2021-01-11 12:04:58 +0000

    reorganised project files

---
+++ public/js/particletypes/torch.js
@@ -0,0 +1,18 @@
+class Torch extends Particle {
+	constructor() {
+		super("torch", [255, 166, 0]);
+	}
+
+	update(x,y,grid) {
+		let changes = [];
+
+		//check if there is air above, if so spawn fire
+		if (y-1 >= 0) {
+			if (grid[x][y-1].type === "air") {
+				changes.push([x,y-1,new Fire0()]);
+
+				return changes;
+			}
+		}
+	}
+}
